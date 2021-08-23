0bfeb9f772878efd9550e4e1f78b2dd58d6f233f
Edwin Ek <isai_ek1@hotmail.com>
2021-08-23 13:00:31 -0500

    modificacion
    modificacion de vistas y rutas

--- routes/web.php
+++ routes/web.php
@@ -17,37 +17,3 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-//Apis Vistas
-
-Route::apiResource('apiSalas','apiSalasController');
-Route::apiResource('apiRecursos','apiRecursosController');
-Route::apiResource('apisolicitudsa','apisolicitudsaController');
-Route::apiResource('apiDocentes','apiDocentesController');
-
-//Vista De Logueo
-Route::view('/','login.logueo');
-
-//Vista De Adminlte
-Route::view('administrador','layouts.adminlte');
-
-//Vista Horarios
-Route::view('horas','horarios.horarios');
-Route::view('recurso','recursos.recursos');
-Route::view('sala','salas.salas');
-Route::view('solicitudre','solicitudre.solicitudre');
-
-//Vista Solicitud Salas
-Route::view('solicitudsa','solicitudsa.solicitudsa');
-
-Route::view('docentes', 'docentes.docentes');
-
-//configuracion
-Route::view('configuracion','docentes.configuracion');
-//historialsolicitudes
-Route::view('solicitudes','docentes.historialsolicitudes.historialsolicituds');
-//paneldesolicitudes
-Route::view('panelsolicitudes','docentes.paneldesolicitudes.panelsolicituds');
-//solicitudes
-Route::view('solicitudrecurso','docentes.solicitudes.solicitudrecurso');
-Route::view('solicitudsala','docentes.solicitudes.solicitudsala');
-
